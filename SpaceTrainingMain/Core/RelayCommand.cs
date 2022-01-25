2939eb59fbd47bba127e2a8cf479f5e37816c8e7
Evgenuy <Evgenuy@EVGENIY>
2022-01-25 13:21:03 +0300

    Добавьте файлы проекта.

---
+++ SpaceTrainingMain/Core/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace SpaceTrainingMain.Core
+{
+    class RelayCommand: ICommand
+    {
+        private Action<object> _execute;
+        private Func<object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object> execute, Func<object, bool> CanExecute = null)
+        {
+            _execute = execute;
+            _canExecute = CanExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null|| _canExecute(parameter);
+        }
+
+        public void Excute(object parameter)
+        {
+            _execute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+
+        }
+    }
+}
---
+++ SpaceTrainingMain/MVVM/ViewMidel/MainViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using SpaceTrainingMain.Core;
+
+namespace SpaceTrainingMain.MVVM.ViewMidel
+{
+    class MainViewModel: ObservableObject
+    {
+        public RelayCommand HomeViewCommand { get; set; }
+
+        public RelayCommand TheoryViewCommand { get; set; }
+
+
+
+        public HomeViewModel HomeVm { get; set; }
+
+        public TheoryViewModel TheoryVm { get; set; }
+
+
+
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        public MainViewModel()
+        {
+            HomeVm = new HomeViewModel();
+            TheoryVm = new TheoryViewModel();
+
+            CurrentView = HomeVm;
+
+            HomeViewCommand = new RelayCommand(o=>
+            {
+                    CurrentView = HomeVm;
+            });
+
+            TheoryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = TheoryVm;
+            });
+
+        }
+
+    }
+}
